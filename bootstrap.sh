830d59af8406d9a046d5f9fa447844917046dc8f
Devin Carr <devin.carr0@gmail.com>
2014-07-07 23:12:17 -0500

    Even more

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-source .bash_profile
-reload
+
+# Check if Homebrew is installed
+which -s brew
+if [[ $? != 0 ]] ; then
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+else
+	brew update
+fi
+
+# Run the Brewfile for more
 brew bundle ./Brewfile
+
+# Move the dotfiles
+for file in ./.{aliases,functions}; do
+	cp $file $HOME;
+done
+
+# Reload the terminal with the new bash_profile
+source ~/.bash_profile
+exec $SHELL -l
