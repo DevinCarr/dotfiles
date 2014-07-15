57ec9d75457b9416a5acca177e74cd4dc0176e22
devincarr <devin.carr0@gmail.com>
2014-07-15 09:56:27 -0500

    updates to aliases and setup

--- min.sh
+++ min.sh
@@ -6,5 +6,8 @@ for file in ./.{aliases,functions,bash_profile,bashrc}; do
 	cp $file $HOME;
 done
 
+mkdir ~/Documents/Github
+mkdir ~/Documents/Code
+
 # Reload the terminal with the new bash_profile
 source ~/.bash_profile
